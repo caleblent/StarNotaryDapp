823c73c99794a4338f7249abe954b1f90c5fa9bd
Caleb Lent <caleb@stcroixwebagency.com>
2022-07-07 13:32:18 -0500

    modified truffle-config.js to work with infura. deployed to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require("truffle-hdwallet-provider")
+const infuraKey = "16af705edfac4e9698332611a6ce3a3e"
+
+const fs = require("fs")
+const mnemonic = fs.readFileSync(".secret").toString().trim()
 
 module.exports = {
   /**
@@ -72,6 +72,13 @@ module.exports = {
     // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4, // rinkeby's id
+      gas: 4500000, // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000,
+    },
 
     // Useful for private networks
     // private: {
